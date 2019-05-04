d21cdb645c611d4e17899e02c48a7d1aa8eeb1f9
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2019-05-04 21:08:46 +0200

    commit

--- src/services/gateway/src/main.ts
+++ src/services/gateway/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import * as compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
